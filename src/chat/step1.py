6eebe26c69259b3bb13eec604579823f5880fb23
Takuya Kinoshita <kktak02@gmail.com>
2025-06-11 06:27:18 +0900

    adding history

--- src/chat/step1.py
+++ src/chat/step1.py
@@ -2,8 +2,17 @@ import streamlit as st
 import os
 import dotenv
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
 
 dotenv.load_dotenv()
+
+MODEL_NAME = "gpt-4.1-nano-2025-04-14"
+
+# セッション状態を初期化
+if "history" not in st.session_state:
+    st.session_state.history = []
+    st.session_state.llm = ChatOpenAI(model_name=MODEL_NAME)
+
 
 
 st.title("マルチモーダルRAGチャットボット")
@@ -16,12 +25,14 @@ if uploaded_file:
 user_input = st.text_input("メッセージを入力してください")
 
 
-
 if st.button("送信"):
-    st.write(f"human: {user_input}")
-    llm = ChatOpenAI(
-        model_name="gpt-4.1-nano-2025-04-14"
-    )
-    response = llm.invoke(user_input)
+    st.session_state.history.append(HumanMessage(user_input))
+    response = st.session_state.llm.invoke(st.session_state.history)
+    st.session_state.history.append(response)
 
-    st.write(f'bot: {response.content}')
+    for message in reversed(st.session_state.history):
+        st.write(f"{message.type}: {message.content}")
+
+
+
+
